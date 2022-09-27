40c32e5fc47bf01f28f27cc4b2fb3e159dbc6fc1
lucianoggm9 <lucianogm1999@gmail.com>
2022-09-27 18:38:07 -0300

    Java

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("hola")
+let nombre= prompt("Ingresa tu nombre de usuario");
+if(nombre == "Luciano"){
+    alert("Bienvenido" +" "+ nombre)
+}
+else if(nombre != "Luciano"){
+    alert("Usuario no encontrado")
+}
+
+const multi  = (a,b) => a * b
+const resta = (a,b) => a - b
+const descuento= 200
+let precioProducto = 500
+let cantidad= prompt("ingresar cantidad")
+let nuevoPrecio =(resta(multi(precioProducto, cantidad), descuento))
+console.log (nuevoPrecio) 
